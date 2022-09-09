e9b8604c58494b7299cc89ff2592e515e90b2504
Ahsan Raza <ahsanrazamemon2000@gmail.com>
2022-09-09 17:09:43 +0500

    Added Car on Canvas.

--- CODE/main.js
+++ CODE/main.js
@@ -8,3 +8,13 @@ canvas.height = window.innerHeight;
 // Set its width 200 Pixels
 canvas.width = 200;
 
+// Step # 1.5: : Draw Car on the Canvas.
+
+// Get the 2D Context of Canvas to draw on Canvas
+const ctx = canvas.getContext("2d");
+
+// Create a Car with 100x, 100y, 30 width & 50 Height.
+const car = new Car(100,100,30,50);
+
+// Draw this Car on Canvas using this context
+car.draw(ctx)
