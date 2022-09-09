71dec5192cd22f7314e894fe2696346a90788a61
Ahsan Raza <ahsanrazamemon2000@gmail.com>
2022-09-09 11:42:34 +0500

    Made Canvas Stretch Full Screen Vertically.

---
+++ CODE/main.js
@@ -0,0 +1,8 @@
+// Get Reference to Canvas with id myCanvas which we defined preiously in HTML.
+const canvas =  document.getElementById("myCanvas");
+
+// Set its height to be the Full window
+canvas.height = window.innerHeight;
+
+// Set its width 200 Pixels
+canvas.width = 200;
